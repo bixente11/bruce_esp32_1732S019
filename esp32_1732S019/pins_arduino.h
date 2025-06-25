f5368fb7fc0058bf3dc230ff4562c95321c49079
bixente11 <peppervideo@gmail.com>
2025-06-25 08:45:42 +0200

    Update pins_arduino.h

--- esp32_1732S019/pins_arduino.h
+++ esp32_1732S019/pins_arduino.h
@@ -11,10 +11,14 @@ static const uint8_t SDA = 47;
 static const uint8_t SCL = 48;
 
 // Modified elsewhere
-static const uint8_t SS = 21;
-static const uint8_t MOSI = 15;
-static const uint8_t MISO = 16;
-static const uint8_t SCK = 17;
+#define SPI_SCK_PIN 17
+#define SPI_MOSI_PIN 15
+#define SPI_MISO_PIN 16
+#define SPI_SS_PIN 19
+static const uint8_t SS = SPI_SS_PIN;
+static const uint8_t MOSI = SPI_MOSI_PIN;
+static const uint8_t SCK = SPI_MISO_PIN;
+static const uint8_t MISO = SPI_SCK_PIN;
 
 #define SERIAL_RX 2
 #define SERIAL_TX 1
@@ -32,7 +36,7 @@ static const uint8_t SCK = 17;
 #define SEL_BTN ENCODER_KEY
 #define UP_BTN -1
 #define DW_BTN -1
-//#define BK_BTN 38     // push button encoder - Back -- double clic
+// #define BK_BTN 38     // push button encoder - Back -- double clic
 #define BTN_ACT LOW
 
 #define RXLED 4
@@ -41,19 +45,19 @@ static const uint8_t SCK = 17;
 #define LED_OFF LOW
 
 #define USE_CC1101_VIA_SPI
-#define CC1101_GDO0_PIN 6
+#define CC1101_GDO0_PIN 38
 #define CC1101_GDO2_PIN 45
-#define CC1101_SS_PIN 38
+#define CC1101_SS_PIN 6
 #define CC1101_MOSI_PIN SPI_MOSI_PIN
 #define CC1101_SCK_PIN SPI_SCK_PIN
 #define CC1101_MISO_PIN SPI_MISO_PIN
 
-#define USE_NRF24_VIA_SPI
-#define NRF24_CE_PIN 18
-#define NRF24_SS_PIN 19
-#define NRF24_MOSI_PIN SPI_MOSI_PIN
-#define NRF24_SCK_PIN SPI_SCK_PIN
-#define NRF24_MISO_PIN SPI_MISO_PIN
+//#define USE_NRF24_VIA_SPI
+//#define NRF24_CE_PIN 18
+//#define NRF24_SS_PIN 19
+//#define NRF24_MOSI_PIN SPI_MOSI_PIN
+//#define NRF24_SCK_PIN SPI_SCK_PIN
+//#define NRF24_MISO_PIN SPI_MISO_PIN
 
 #define FP 1
 #define FM 2
@@ -85,11 +89,6 @@ static const uint8_t SCK = 17;
 #define SDCARD_SCK 17
 #define SDCARD_MISO 16
 #define SDCARD_MOSI 15
-
-#define SPI_SCK_PIN 17
-#define SPI_MOSI_PIN 15
-#define SPI_MISO_PIN 16
-#define SPI_SS_PIN 43
 
 // === I2C Grove ===
 #define GROVE_SDA SDA
